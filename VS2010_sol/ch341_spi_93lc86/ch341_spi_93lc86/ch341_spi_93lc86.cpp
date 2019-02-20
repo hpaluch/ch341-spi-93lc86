3728a0d51042b1dfd0bc02de6a8f71e9b63e7d6a
Henryk Paluch <hpaluch@seznam.cz>
2019-02-20 18:12:37 +0100

    READ finished - now dumps whole 93LC86 to screen

--- VS2010_sol/ch341_spi_93lc86/ch341_spi_93lc86/ch341_spi_93lc86.cpp
+++ VS2010_sol/ch341_spi_93lc86/ch341_spi_93lc86/ch341_spi_93lc86.cpp
@@ -14,47 +14,137 @@
 #define HPCH_MASK_CS0  (1 << HPCH_BIT_CS0)
 #define HPCH_MASK_CLK  (1 << HPCH_BIT_CLK)
 #define HPCH_MASK_MOSI (1 << HPCH_BIT_MOSI)
+#define HPCH_MASK_MISO (1 << HPCH_BIT_MISO)
 
-static void DumpByteArray(BYTE *b, int n){
-	int i=0;
-	for(int i=0;i<n;i++){
-		printf("Index=%d, Value=0x%x\r\n",i,(unsigned)b[i]);
-	}
-}
-
-#define READ_CMD_BITS 22
-
-static BYTE cmdBuf[READ_CMD_BITS+1] = {0};
 
 #define PREFIX_BITS 3
 #define ADDR_BITS 11
+#define ADDR_MASK 0x7ff
 #define DATA_BITS  8
+#define DATA_MASK 0xff
 
-static void PrepareReadCommand(){
-	DWORD cmdBits = READ_CMD_BITS;
-	DWORD addr = 3;
 
-	DWORD cmd = 0x6; // READ command is 1|10|
-	cmd <<= ADDR_BITS;
-	cmd |=  addr;
-	cmd <<= DATA_BITS;
+// max total bits in bit-stream = bits in DWORD (this program limitation)
+#define HPCH_93C_MAX_BITS 32
 
-	for(int i=0;i<READ_CMD_BITS;i++){
+//
+// Send 93LC86 command and returns response
+// DWORD iIndex - CH341A device SN, or 0 for 1st device
+// DWORD inData - input data bits
+// DWORD nBits  - number of bits to send in inData
+// DWORD csBits - number of CS off bits to send (can be 0 to leave CS ON)
+// DWORD outData - data bits returned from 93LC86 - only nBits inserted...
+BOOL HpCh_93c_SendCommand(ULONG iIndex, DWORD inData,DWORD nBits, DWORD csBits,DWORD *outData){
+	BOOL ret = FALSE;
+	int i=0;
+	static BYTE ioBuf[HPCH_93C_MAX_BITS];
+
+	if (outData == NULL ){
+		fprintf(stderr,"Parameter outData may not be NULL\n");
+		return FALSE;
+	}
+
+	if (nBits + csBits > HPCH_93C_MAX_BITS){
+		fprintf(stderr,"nBits %u + csBits %u = %u exceeds limit %u\n",nBits,csBits,nBits+csBits, HPCH_93C_MAX_BITS);
+		return FALSE;
+	}
+	// ensure consistent data in buffer
+	memset(ioBuf,0,sizeof(ioBuf));
+
+	for(i=0;i<(int)nBits;i++){
 		BYTE b = 0;
-		BOOL isBitSet = ( cmd & (1 << (READ_CMD_BITS-i-1))) ? TRUE : FALSE;
+		BOOL isBitSet = ( inData & (1 << (nBits-i-1))) ? TRUE : FALSE;
 
 		b |= HPCH_MASK_CS0; // activate CS0
 		if (isBitSet){
-			b |= HPCH_MASK_MOSI;
+			b |= HPCH_MASK_MOSI; // MOSI data output if bit is set
 		}
-		cmdBuf[i] = b;
+		ioBuf[i] = b;
 	}
-	cmdBuf[READ_CMD_BITS] = 0; // hack de-activate CS0 at the end...
+
+#if 0
+	// these data are alrady off
+	for(i=0;i<csBits;i++){
+		ioBuf[i+nBits] = 0; // just for reference - CS off, MOSI off
+	}
+#endif
+	if (!CH341BitStreamSPI(iIndex,nBits+csBits,ioBuf)){
+		fprintf(stderr,"CH341BitStreamSPI() failed\n");
+		return FALSE;
+	}
+
+	// now transfer MISO in Bits back to *outData - only nBits transferred
+	*outData = 0;
+	for(i=0;i<(int)nBits;i++){
+		*outData <<= 1;
+		BYTE b = ioBuf[i];
+		if ( b & HPCH_MASK_MISO ){
+			*outData |= 1;
+		}
+	}
+	return TRUE;
 }
 
+BOOL HpCh_93c_Read(ULONG iIndex,DWORD addr,DWORD *outData){
+	DWORD cmd = 0;
+
+	if (outData == NULL ){
+		fprintf(stderr,"Parameter outData may not be NULL\n");
+		return FALSE;
+	}
+	// READ command is 1|10| => 0x6
+	cmd = 0x6 << ( ADDR_BITS + DATA_BITS ); 
+	if ( addr & ~ ADDR_MASK ){
+		fprintf(stderr,"addr 0x%x is too large. Maximum allowed is: 0x%x\n",addr, ADDR_MASK);
+		return FALSE;
+	}
+	cmd |= (addr << DATA_BITS);
+	// we use 2 CS off bits to padd data to 2-bit for USB adapter
+	if (!HpCh_93c_SendCommand(iIndex, cmd,(ADDR_BITS+DATA_BITS+PREFIX_BITS), 2, outData)){
+		return FALSE;
+	}
+	// verify that A0 MISO is 0
+	if ( *outData & ( 1 << DATA_BITS) ){
+		fprintf(stderr,"MISO response on A0 address bit is not 0\n");
+		return FALSE;
+	}
+	*outData &= DATA_MASK; // cut off all non-data bits from input
+
+	return TRUE;
+}
+
+void HpCh_DumpBuf(BYTE *buf, int n){
+	const int VALUES_PER_LINE = 16;
+
+	int i=0;
+	printf("Dump of buffer at 0x%p,  bytes %u\n",buf,n);
+	for(i=0;i<n;i++){
+		if ( i % VALUES_PER_LINE == 0){
+			// dump also ASCII values
+			if (i >= VALUES_PER_LINE ){
+				int j=0;
+				// XXX: puts(3) always append \n
+				putc(' ',stdout);
+				for(j=0;j<VALUES_PER_LINE;j++){
+					BYTE b = buf[i-VALUES_PER_LINE+j];
+					if (b>=32 && b<127){
+						putc(b,stdout);
+					} else {
+						putc('.',stdout);
+					}
+				}				
+			}
+			printf("\n%03x",i);
+		}
+		printf(" %02x",buf[i]);
+	}
+	printf("\n");
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int addr = 0;
+	BYTE dataBuf[ADDR_MASK+1];
 	int ret = EXIT_SUCCESS;
 	HANDLE h341 = NULL;
 	ULONG iDevIndex = 0; // first device
@@ -79,44 +169,28 @@ int _tmain(int argc, _TCHAR* argv[])
 			goto exit1;
 	}
 
+	// the while is here only to have repeated output to Logic Analyzer
 	while(1){
-		BYTE bufCs0 = HPCH_MASK_CS0;
-
-		PrepareReadCommand();
-		printf("Sending Bit-Stream:\r\n");
-		DumpByteArray(cmdBuf,READ_CMD_BITS+1);
-		if (!CH341BitStreamSPI(iDevIndex,READ_CMD_BITS+1,cmdBuf)){
-			printf("CH341BitStreamSPI failed\r\n");
-			ret = EXIT_FAILURE;
-			goto exit1;
+		memset(dataBuf,0,sizeof(dataBuf));
+		printf("Reading %u bytes from 93LC86...\n",sizeof(dataBuf));
+		for(addr=0; addr<=ADDR_MASK;addr++){
+			DWORD b = 0;
+			if (!HpCh_93c_Read(iDevIndex,addr,&b)){
+				ret = EXIT_FAILURE;
+				goto exit1;
+			}
+			// currently we expect 8-bit data (organization of 93LC86)
+			if (b & ~0xff){
+				fprintf(stderr,"Got too large value %u, maximum byte %u expected\n",b,255);
+				ret = EXIT_FAILURE;
+				goto exit1;
+			}
+			dataBuf[addr] = (BYTE)b;
 		}
-		printf("SPI returned back data:\r\n");
-		DumpByteArray(cmdBuf,READ_CMD_BITS+1);
-
+		printf("Done. Data dump follows:\n");
+		HpCh_DumpBuf(dataBuf,sizeof(dataBuf));
 		Sleep(5);
-
-		/*
-		if (!CH341SetDelaymS(iDevIndex,5)){
-			printf("CH34SetDelaymS failed\r\n");
-			ret = EXIT_FAILURE;
-			goto exit1;
-		}
-		printf("SPI CS0 buf:\r\n");
-		DumpByteArray(&bufCs0,1);
-		if (!CH341BitStreamSPI(iDevIndex,1,&bufCs0)){
-			printf("CH341BitStreamSPI failed\r\n");
-			ret = EXIT_FAILURE;
-			goto exit1;
-		}
-		printf("SPI CS0 returned buf:\r\n");
-		DumpByteArray(&bufCs0,1);
-		if (!CH341SetDelaymS(iDevIndex,2)){
-			printf("CH34SetDelaymS failed\r\n");
-			ret = EXIT_FAILURE;
-			goto exit1;
-		}
-		*/
-		//break; // comment it out to have stream of data - for Logic Analyzer
+		break; // comment it out to have stream of data - for Logic Analyzer
 	}
 
 exit1:
